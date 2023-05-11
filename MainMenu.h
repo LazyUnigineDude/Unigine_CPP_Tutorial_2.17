c4eb4b884a7a334d93d5d5ddf5cefbf2103cfc4d
LazyUnigineDude <lazy.unigine@gmail.com>
2023-05-10 23:21:49 -0400

    Add

--- MainMenu.h
+++ MainMenu.h
@@ -7,9 +7,23 @@ public:
 	COMPONENT_DEFINE(MainMenu, ComponentBase)
 		COMPONENT_INIT(Init)
 		COMPONENT_UPDATE(Update)
+		COMPONENT_SHUTDOWN(ShutDown)
+		PROP_PARAM(Toggle, isMainMenu)
+		PROP_PARAM(String, WorldChangerName)
 		PROP_PARAM(File, WorldChanger)
+
+		void MenuOpened(bool isOpen);
+
 protected:
 	void Init();
 	void Update();
+	void ShutDown();
+
+private:
+	void onClicked();
+	void onQuit();
+	bool isPaused;
+	Unigine::WidgetButtonPtr Button, QuitButton;
+	Unigine::GuiPtr GUI = Unigine::Gui::getCurrent();
 };
 
