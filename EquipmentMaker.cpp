2f9acf8eef80fb67c3236d46aaecf79324b3839f
LazyUnigineDude <lazy.unigine@gmail.com>
2023-08-02 19:47:31 -0400

    add

--- EquipmentMaker.cpp
+++
@@ -1,7 +0,0 @@
-#include "EquipmentMaker.h"
-REGISTER_COMPONENT(EquipmentMaker)
-
-void EquipmentMaker::Init() {
-
-	Equipment = InventoryMaker(EquipmentNode->getProperty(0)->getParameterPtr(0));
-}
